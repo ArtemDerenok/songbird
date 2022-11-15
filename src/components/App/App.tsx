bfb17344b4618ef11761a40f4ce8fdb85ad7a5f1
Artem <derenokartem95@gmail.com>
2022-11-15 14:56:13 +0300

    feat: add styles and structure

---
+++ src/components/App/App.tsx
@@ -0,0 +1,12 @@
+import Header from "../Header/Header";
+import styles from "./App.module.scss";
+
+function App() {
+  return (
+    <div className={styles.container}>
+      <Header />
+    </div>
+  );
+}
+
+export default App;
