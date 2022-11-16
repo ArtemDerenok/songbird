dea64bd29a0e2c08c6659e12efb135efe084a955
Artem <derenokartem95@gmail.com>
2022-11-16 16:03:40 +0300

    feat: add question bar component

---
+++ src/components/QuestionBar/QuestionBar.tsx
@@ -0,0 +1,11 @@
+import styles from "./QuestionBar.module.scss";
+
+const QuestionBar = () => {
+  return (
+    <section className={styles.container}>
+      <img src="unknownBird.jpg" alt="bird" />
+    </section>
+  );
+};
+
+export default QuestionBar;
