4e95610ce304e9274f4e2f17f7783a1032212ad6
Artem <derenokartem95@gmail.com>
2022-11-16 16:04:35 +0300

    feat: add nextlevel component

---
+++ src/components/NextLevelBtn/NextLevelBtn.tsx
@@ -0,0 +1,11 @@
+import styles from "./NextLevelBtn.module.scss";
+
+const NextLevelBtn = () => {
+  return (
+    <button className={styles.button} type="button">
+      Следующий уровень
+    </button>
+  );
+};
+
+export default NextLevelBtn;
