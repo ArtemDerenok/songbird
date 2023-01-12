1a28ad236f9848a965eda53a60e9335e0eccd6a3
Artem <derenokartem95@gmail.com>
2023-01-12 17:03:39 +0300

    feat: add infoBar logic

--- src/components/InfoBar/InfoBar.tsx
+++ src/components/InfoBar/InfoBar.tsx
@@ -1,12 +1,35 @@
+import { IBirdInfo } from "../../types/interfaces";
 import styles from "./InfoBar.module.scss";
 
-const InfoBar = () => {
+interface IInfoBird {
+  birdInfo: IBirdInfo;
+}
+
+const InfoBar = ({ birdInfo }: IInfoBird) => {
   return (
     <section className={styles.container}>
-      <p>
-        Послушайте плеер.<br></br>
-        Выберите птицу из списка
-      </p>
+      {birdInfo.isBegin === true ? (
+        <p>
+          Послушайте плеер.<br></br>
+          Выберите птицу из списка
+        </p>
+      ) : (
+        <div className={styles.infoBar_table}>
+          <div className={styles.infoBar_table_box_one}>
+            <img
+              className={styles.image}
+              src={birdInfo.bird?.image}
+              alt={birdInfo.bird?.name}
+            />
+            <div className={styles.infoBar_table_box_two}>
+              <h3>{birdInfo.bird?.name}</h3>
+              <h5>{birdInfo.bird?.species}</h5>
+              <audio src={birdInfo.bird?.audio} controls></audio>
+            </div>
+          </div>
+          <p>{birdInfo.bird?.description}</p>
+        </div>
+      )}
     </section>
   );
 };
