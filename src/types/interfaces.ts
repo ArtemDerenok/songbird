39fd0b94498905929bb5098b36b75cc49c74d651
Artem <derenokartem95@gmail.com>
2023-01-12 17:38:43 +0300

    feat: add interfaces

--- src/types/interfaces.ts
+++ src/types/interfaces.ts
@@ -15,6 +15,19 @@ interface IQuestionsSate {
     image: string,
     audio: string,
   }[],
+  round: number,
+}
+
+export interface IBirdInfo {
+  bird: {
+    id: number,
+    name: string,
+    species: string,
+    description: string,
+    image: string,
+    audio: string,
+  } | undefined,
+  isBegin: boolean,
 }
 
 export default IQuestionsSate;
