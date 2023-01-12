319aed3e53f40b0a5b2eefcf4a67a729bfc001f5
Artem <derenokartem95@gmail.com>
2023-01-12 17:06:26 +0300

    feat: add app logic

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,10 +1,28 @@
 import Header from "../Header/Header";
+import Main from "../pages/Main";
 import styles from "./App.module.scss";
+import { useState, useCallback } from "react";
 
 function App() {
+  const [score, setScore] = useState<number>(0);
+  const [attemptsAnswer, setAttemptsAnswer] = useState<number>(5);
+
+  const changeAttemtsAnswer = useCallback(() => {
+    setAttemptsAnswer(attemptsAnswer - 1);
+  }, [attemptsAnswer]);
+
+  const changeScore = useCallback(() => {
+    setScore(score + attemptsAnswer);
+    setAttemptsAnswer(5);
+  }, [attemptsAnswer, score]);
+
   return (
     <div className={styles.container}>
-      <Header />
+      <Header score={score} />
+      <Main
+        changeAttemtsAnswer={changeAttemtsAnswer}
+        changeScore={changeScore}
+      />
     </div>
   );
 }
