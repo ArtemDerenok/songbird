3f071583e141ea855301640cca9f0697bcef8644
Artem <derenokartem95@gmail.com>
2023-01-12 17:05:40 +0300

    feat: add question bar logic

--- src/components/QuestionBar/QuestionBar.tsx
+++ src/components/QuestionBar/QuestionBar.tsx
@@ -1,9 +1,35 @@
+import useTypeSelector from "../../hooks/useTypeSelector";
 import styles from "./QuestionBar.module.scss";
 
-const QuestionBar = () => {
+interface IQuestionBar {
+  rightAnswerId: number;
+  isRightAnswer: boolean;
+}
+
+const QuestionBar = ({ rightAnswerId, isRightAnswer }: IQuestionBar) => {
+  const question = useTypeSelector((state) =>
+    state.currentGame.find((elem) => elem.id === rightAnswerId)
+  );
+
   return (
     <section className={styles.container}>
-      <img src="unknownBird.jpg" alt="bird" />
+      {isRightAnswer === false ? (
+        <>
+          <img src="unknownBird.jpg" alt="bird" />
+          <div>
+            <p>******</p>
+            <audio src={question?.audio} controls></audio>
+          </div>
+        </>
+      ) : (
+        <>
+          <img src={question?.image} alt={question?.name} />
+          <div>
+            <p>{question?.name}</p>
+            <audio src={question?.audio} controls></audio>
+          </div>
+        </>
+      )}
     </section>
   );
 };
