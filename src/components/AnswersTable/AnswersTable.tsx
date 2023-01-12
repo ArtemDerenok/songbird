b04bcc22d7391c191da50d63716e54c55543a905
Artem <derenokartem95@gmail.com>
2023-01-12 17:02:32 +0300

    feat: add answer table logic

--- src/components/AnswersTable/AnswersTable.tsx
+++ src/components/AnswersTable/AnswersTable.tsx
@@ -1,14 +1,42 @@
+import { IBirdInfo } from "../../types/interfaces";
+import useTypeSelector from "./../../hooks/useTypeSelector";
 import styles from "./AnswersTable.module.scss";
 
-const AnswersTable = () => {
+interface IAnswerTable {
+  handleBirdInfo: (data: IBirdInfo) => void;
+  checkRightAnswer: (id: number | undefined) => void;
+  isRightAnswer: boolean;
+}
+
+const AnswersTable = ({
+  handleBirdInfo,
+  checkRightAnswer,
+  isRightAnswer,
+}: IAnswerTable) => {
+  const questions = useTypeSelector((state) => state.currentGame);
+
   return (
     <ul className={styles.list}>
-      <li>Ворон</li>
-      <li>Журавль</li>
-      <li>Ласточка</li>
-      <li>Козодой</li>
-      <li>Кукушка</li>
-      <li>Синица</li>
+      {questions.map((elem) => (
+        <li
+          onClick={(e) => {
+            if (!isRightAnswer) {
+              const data = {
+                bird: questions.find(
+                  (elem) => elem.name === e.currentTarget.innerText
+                ),
+                isBegin: false,
+              };
+
+              handleBirdInfo(data);
+              checkRightAnswer(data.bird?.id);
+            }
+          }}
+          key={elem.id}
+        >
+          {elem.name}
+        </li>
+      ))}
     </ul>
   );
 };
