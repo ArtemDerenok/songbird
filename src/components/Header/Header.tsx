f25765328e85e93d27b61dea38bdb13685c62ae5
Artem <derenokartem95@gmail.com>
2023-01-12 17:03:18 +0300

    feat: add header logic

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,19 +1,42 @@
+import { useMemo } from "react";
+import useTypeSelector from "../../hooks/useTypeSelector";
 import styles from "./Header.module.scss";
 
-const Header = () => {
+interface IHeader {
+  score: number;
+}
+
+const Header = ({ score }: IHeader) => {
+  const round = useTypeSelector((state) => state.round);
+  const roundNames = useMemo(
+    () => [
+      "Разминка",
+      "Воробьиные",
+      "Лесные птицы",
+      "Певчие птицы",
+      "Хищные птицы",
+      "Морские птицы",
+    ],
+    []
+  );
+
   return (
     <header className={styles.header}>
       <div className={styles.top_panel}>
         <div className={styles.top_panel_logo}></div>
-        <p className={styles.top_panel_score}>Score: 0</p>
+        <p className={styles.top_panel_score}>Score: {score}</p>
       </div>
       <ul className={styles.pagination}>
-        <li className={styles.pagination_item}>Разминка</li>
-        <li className={styles.pagination_item}>Воробьиные</li>
-        <li className={styles.pagination_item}>Лесные птицы</li>
-        <li className={styles.pagination_item}>Певчие птицы</li>
-        <li className={styles.pagination_item}>Хищные птицы</li>
-        <li className={styles.pagination_item}>Морские птицы</li>
+        {roundNames.map((elem, index) => (
+          <li
+            key={index}
+            className={`${styles.pagination_item} ${
+              round === index ? styles.active : ""
+            }`}
+          >
+            {elem}
+          </li>
+        ))}
       </ul>
     </header>
   );
