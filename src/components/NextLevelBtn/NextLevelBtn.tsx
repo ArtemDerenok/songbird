23b81bd14b4a9d0a13a2408ba8eedbdee01baf4d
Artem <derenokartem95@gmail.com>
2023-01-12 17:04:06 +0300

    feat: add next level btn logic

--- src/components/NextLevelBtn/NextLevelBtn.tsx
+++ src/components/NextLevelBtn/NextLevelBtn.tsx
@@ -1,8 +1,45 @@
+import { useDispatch } from "react-redux";
+import {
+  changeRoundActionCreator,
+  resetRoundActionCreator,
+} from "../../redux/reducers/questionsReducer";
 import styles from "./NextLevelBtn.module.scss";
+import useTypeSelector from "./../../hooks/useTypeSelector";
 
-const NextLevelBtn = () => {
+interface INextLevelBtn {
+  isRightAnswer: boolean;
+  changeIsRightAnswer: () => void;
+  changeRightAnswerId: () => void;
+}
+
+const NextLevelBtn = ({
+  isRightAnswer,
+  changeIsRightAnswer,
+  changeRightAnswerId,
+}: INextLevelBtn) => {
+  const dispatch = useDispatch();
+  const round = useTypeSelector((state) => state.round);
+
   return (
-    <button className={styles.button} type="button">
+    <button
+      className={`${styles.button} ${
+        isRightAnswer === true ? styles.active : ""
+      }`}
+      type="button"
+      onClick={() => {
+        if (isRightAnswer) {
+          changeIsRightAnswer();
+          changeRightAnswerId();
+
+          if (round === 5) {
+            dispatch(resetRoundActionCreator());
+          } else {
+            dispatch(changeRoundActionCreator());
+          }
+        }
+        return false;
+      }}
+    >
       Следующий уровень
     </button>
   );
